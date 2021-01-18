3886ef6544ab57db7be67cf97426b209450218b4
matthew <matthewchuang@gmail.com>
2021-01-18 10:29:01 +0800

    with tensorboard

--- copula/main_fsm.py
+++ copula/main_fsm.py
@@ -1,7 +1,7 @@
 
 import enum
 import operator
-
+import logbook
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -16,38 +16,52 @@ from zipline.finance import commission, slippage
 from fsm_states import s
 from helper import compare_array_with_float, find_Return
 import math
+import sys
+
+from tensorboard import TensorBoard
+
+# zipline_logging = logbook.NestedSetup([
+#     logbook.NullHandler(level=logbook.DEBUG, bubble=True),
+#     logbook.StreamHandler(sys.stdout, level=logbook.INFO),
+#     logbook.StreamHandler(sys.stderr, level=logbook.ERROR),
+# ])
+# zipline_logging.push_application()
 
 
-class s(enum.Enum): #States
-    DEFAULT         = 0
-    L0S1            = 1  #Long sym[0] short sym[1]
-    L1S0            = 2  #Long sym[1] short sym[0]   
-    pass
 
-'''             Trigger             Prev State          Next State'''
+# class s(enum.Enum): #States
+#     DEFAULT         = 0
+#     L0S1            = 1  #Long sym[0] short sym[1]
+#     L1S0            = 2  #Long sym[1] short sym[0]   
+#     pass
 
-transitions_ = [["initiate",        s.DEFAULT ,         s.DEFAULT],
-                ["order_0",         "*",                  s.L0S1],
-                ["release",         s.L0S1,             s.DEFAULT],
-                ["order_1",         "*",                  s.L1S0],
-                ["release",         s.L1S0,             s.DEFAULT]
-                                                                            ]
+# '''             Trigger             Prev State          Next State'''
+
+# transitions_ = [["initiate",        s.DEFAULT ,         s.DEFAULT],
+#                 ["order_0",         "*",                  s.L0S1],
+#                 ["release",         s.L0S1,             s.DEFAULT],
+#                 ["order_1",         "*",                  s.L1S0],
+#                 ["release",         s.L1S0,             s.DEFAULT]
+#                                                                             ]
 
 def initialize(env):
     # set_max_leverage(3.3)
     set_benchmark(False)
     env.capital_base = 100000
-    env.sym = [symbol("NSC"),symbol("CSX")]
+    env.sym = [symbol("HQL"),symbol("HQH")]
     env.day_count = 0
-    env.floor_CL = 0.15
-    env.cap_CL = 0.85
+    env.floor_CL = 0.2
+    env.cap_CL = 0.8
     env.set_commission(commission.PerShare(cost=.0075, min_trade_cost=1.0))
     env.set_slippage(slippage.VolumeShareSlippage())
     env.leverage_flag = 0
     env.model = ""
     env.long_short_percentage = 1
-    env.m = Machine(states=s, transitions=transitions_,initial=s.DEFAULT)
-    env.lookback =20 
+    env.control =0
+    # env.m = Machine(states=s, transitions=transitions_,initial=s.DEFAULT)
+
+    
+    env.tensorboard = TensorBoard(log_dir= "/Users/matthewchuang/Documents/GitHub/algo_test/copula/test")
     pass
 
 
@@ -93,17 +107,6 @@ def handle_data(env, data):
     ret_1 = ret.iloc[:,0]
     ret_2 = ret.iloc[:,1]
 
-    #DM
-    df1 = df.iloc[-env.lookback:,:]
-    spread_mean = (df1.iloc[:,1]-df1.iloc[:,0]).mean()
-    spread_std = (df1.iloc[:,1]-df1.iloc[:,0]).std()
-
-    upper_lim = spread_mean + 1.5 * spread_std
-    lower_lim = spread_mean - 1.5 * spread_std
-    up_exit =   spread_mean - 0.5 * spread_std
-    low_exit =  spread_mean - 0.5*  spread_std 
-
-    spread = df.iloc[-1,1] - df.iloc[-1,0]
     # copula
     tau_ = kendalltau(ret_1,ret_2)[0]
     if env.day_count % 1000 == 1:
@@ -119,64 +122,63 @@ def handle_data(env, data):
 
     # Misprice index 
 
-    MI_u_v = env.model.partial_derivative(u_v_stack)[-1:]
-    MI_v_u = env.model.partial_derivative(v_u_stack)[-1:]
+    MI_u_v = env.model.partial_derivative(u_v_stack)
+    MI_v_u = env.model.partial_derivative(v_u_stack)
 
 
     #fsm to check order state 
+    #env.control = blah
 
-    if env.m.state == s.DEFAULT:
+
+    if env.control == 0:
         print("DEFAULT")
-        if compare_array_with_float(MI_u_v, env.floor_CL,"<") and compare_array_with_float(MI_v_u, env.cap_CL,">") and spread > upper_lim:
+        if sum(i < env.floor_CL for i in MI_u_v ) > 1 and sum(i > env.cap_CL for i in MI_v_u ) > 1 :
             #long u short v
             long_pos = order_target_percent(env.sym[0], env.long_short_percentage)
             short_pos = order_target_percent(env.sym[1], -env.long_short_percentage )
-            env.m.order_0()
-            env.lookback +=1
+            env.control = 1
+            
             print(-116)
 
             # Placing orders: if short is relatively underpriced, sell the pair
-        elif compare_array_with_float(MI_u_v, env.cap_CL,">") and compare_array_with_float(MI_v_u, env.floor_CL,"<") and spread < lower_lim:
+        elif sum(i > env.cap_CL for i in MI_u_v ) > 1 and sum(i < env.floor_CL for i in MI_v_u ) > 1:
             #short u long v
             long_pos = order_target_percent(env.sym[1], env.long_short_percentage )
             short_pos = order_target_percent(env.sym[0], -env.long_short_percentage )
-            env.m.order_1()
-            env.lookback +=1
+            env.control = 2
+            
             print(-123)
         
         pass
 
-    elif env.m.state == s.L0S1:
+    elif env.control == 1:
         print("LOS1")
-        if compare_array_with_float(MI_u_v, env.cap_CL,">") and compare_array_with_float(MI_v_u, env.floor_CL,"<") and spread < lower_lim:
+        if sum(i > env.cap_CL for i in MI_u_v ) > 1 and sum(i < env.floor_CL for i in MI_v_u ) > 1:
             long_pos = order_target_percent(env.sym[0], env.long_short_percentage)
             short_pos = order_target_percent(env.sym[1], -env.long_short_percentage )
-            env.m.order_1()
-            env.lookback = 21
+            env.control = 2
+
             print(-116)
-        elif spread  < up_exit:
+        elif sum(k > 0.2 for k in MI_u_v) > 1  and sum(k < 0.8 for k in MI_v_u) > 1:
             short_pos = order_target(env.sym[1], 0)
             long_pos = order_target(env.sym[0], 0)
-            env.m.release()
-            env.lookback = 20
-        else:
-            env.lookback +=1
-    elif env.m.state == s.L1S0:
+            env.control = 0
+
+
+    elif env.control == 2:
         print("L1S0")
-        if compare_array_with_float(MI_u_v, env.cap_CL,">") and compare_array_with_float(MI_v_u, env.floor_CL,"<") and spread < lower_lim:
+        if sum(i < env.floor_CL for i in MI_u_v ) > 1 and sum(i > env.cap_CL for i in MI_v_u ) > 1 :
             #short u long v
             long_pos = order_target_percent(env.sym[1], env.long_short_percentage )
             short_pos = order_target_percent(env.sym[0], -env.long_short_percentage )
-            env.m.order_0()
-            env.lookback = 21
+            env.control = 1
+
             print(-123)
-        elif spread > low_exit:
+        elif sum(k < 0.8 for k in MI_u_v) > 1  and sum(k >0.2 for k in MI_v_u) > 1:
             short_pos = order_target(env.sym[1], 0)
             long_pos = order_target(env.sym[0], 0)
-            env.m.release()
-            env.lookback = 20
-        else:
-            env.lookback +=1
+            env.control = 0
+
 
 
     #loggeresss
@@ -188,11 +190,6 @@ def handle_data(env, data):
     print("MI_u_v:",MI_u_v)
     print("MI_v_u:",MI_v_u)
 
-    print("spread:",spread)
-    print("mean_spread",spread_mean)
-    print("up_lim", upper_lim)
-    print("lower_lim", lower_lim)
-
 
     order1 = get_open_orders(env.sym[0])
     order2 = get_open_orders(env.sym[1])
@@ -201,7 +198,7 @@ def handle_data(env, data):
     if order1 != [] and order2 != []:
         print("Amount:", order1[0]["amount"])
         print("Amount:", order2[0]["amount"])
-
+    env.tensorboard.log_algo(env)
     pass
 
 
@@ -209,7 +206,7 @@ def handle_data(env, data):
 
 # Fire off backtest
 
-start = pd.Timestamp('2000-11-18', tz='utc')
+start = pd.Timestamp('2012-11-18', tz='utc')
 end = pd.Timestamp('2021-1-4', tz='utc')
 
 result = run_algorithm(
