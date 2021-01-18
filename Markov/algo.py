7b830c10f25db6422861b28e583a0852c24e9242
matthew <matthewchuang@gmail.com>
2021-01-18 19:48:38 +0800

    a markov chain model that cannot earn money with a terrible stock but can beat the stock itself if the stock itself is bullish

--- Markov/algo.py
+++ Markov/algo.py
@@ -3,23 +3,21 @@ from zipline import run_algorithm
 from zipline.api import *
 from zipline.finance import commission, slippage
 
-from zipline.finance import (commission)
-
-import operator
-
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from markovfsm import Chain
+from markovfsm.plot import transitions_to_graph
+from graphviz import Digraph
 
-from helper import find_Return, compare_array_with_float , put_into_bin
+from tensorboard import TensorBoard
+from helper import find_Return, compare_array_with_float , put_into_bin, expected_value 
 
 def initialize(env):
     # set_max_leverage(3.3)
     set_benchmark(False)
     env.capital_base = 100000
-    env.sym = symbol("AAPL")
+    env.sym = symbol("MTN")
     env.day_count = 0
     env.floor_CL = 0.05
     env.cap_CL = 0.95
@@ -29,6 +27,7 @@ def initialize(env):
     env.model = ""
     env.long_short_percentage = 1
     env.hold_flag = 0
+    env.tensorboard = TensorBoard(log_dir= "/Users/matthewchuang/Documents/GitHub/algo_test/Markov/test")
     pass
 
 
@@ -45,25 +44,40 @@ def handle_data(env, data):
         h = ret.std() *3.49 / len(ret)**(1/3)
         ret_list = ret.tolist()
         bins , slices = put_into_bin(ret_list, h)
-
-        chain = Chain(len(bins), bins[0])
+        chain = Chain(max(bins)+1, bins[0])
         for i in range(1,len(bins)):
             chain.learn(bins[i])
         prob = chain.get_transitions_probs(bins[-1])
         
-        print(bins)
         pos = 0
-        for i in range(len(slices)):
-            if slices[i] > 0:
-                print(slices[i-1])
-                print(slices[i])
-                print(slices[i+1])
-                pos = i 
-                break
-        print(pos)
-        print(bins[pos])
-        print(set(bins))
 
+
+        prob = chain.get_transitions_probs(bins[-1])
+        ev = expected_value(slices, prob)
+        if ev>0:
+            order_target_percent(env.sym,1)
+        else:
+            order_target_percent(env.sym,-1)
+
+        #print
+
+        print("--------------------------------")
+        print(df.index[-1])
+        print("PNL: ", env.portfolio.pnl)
+        print(ev)
+        order  = get_open_orders(env.sym)
+        print(order)
+        if order != []:
+            print("Amount:", order[0]["amount"])
+        
+        env.tensorboard.log_algo(env)
+
+
+
+        record(P=data.current(env.sym,"price"))
+        record(EV = ev)
+
+        
 
 
     env.day_count+=1
@@ -87,7 +101,7 @@ result = run_algorithm(
     data_frequency = 'daily',  # Set data frequency
     bundle='daily-bundle' ) # Select bundle
 
-# result.to_pickle("test.pkl")
+result.to_pickle("test4.pkl")
 
     #zipline run -f main.py -o test.csv -s 2000-11-18 -e 2021-1-4 -b custom-bundle --no-benchmark --capital-base 100
     #zipline run -f main.py -o test.csv -s 2000-11-18 -e 2021-1-4 -b custom-bundle --no-benchmark 
@@ -100,3 +114,5 @@ result = run_algorithm(
 
 
 # ssh -i ~/Downloads/algot.pem ubuntu@ec2-18-162-232-172.ap-east-1.compute.amazonaws.com
+
+#tensorboard --logdir=test/ --host localhost --port 8088
--- markov/helper.py
+++ markov/helper.py
@@ -27,14 +27,24 @@ def put_into_bin(arr , w):
     min_ = min(arr)
     catagory.append(min_)
     catagory.append(min_+ rem)
-    for i in range(2,num_of_bin-1):
+    for i in range(2,num_of_bin-2):
         catagory.append(min_ + i * w)
     bin_indices = np.digitize(arr, catagory)
     return bin_indices , catagory
 
 
 
-
+def expected_value(slices, prob):
+    from numpy import std,mean
+    n = len(slices)
+    list_ =[]
+    std_ = std(slices)
+    mean_ =mean(slices)
+    list_.append(mean_-2*std_)
+    for i in range(n-1):
+        list_.append((slices[i+1]+slices[i+1])/2)
+    list_.append(mean_ + 2*std_)
+    return np.dot(list_,prob)
 
 
     
