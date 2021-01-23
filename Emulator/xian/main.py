0712c82fe16d830fb93f6fcd589d2e300da97043
XianNg24 <49993481+XianNg24@users.noreply.github.com>
2021-01-23 10:11:39 +0800

    string to dict

--- Emulator/xian/main.py
+++ Emulator/xian/main.py
@@ -4,7 +4,6 @@ from  PyQt5.uic  import  loadUi
 from  matplotlib.backends.backend_qt5agg  import  ( NavigationToolbar2QT  as  NavigationToolbar )
 
 import  numpy  as  np
-import  random
 
 class  MatplotlibWidget ( QMainWindow ):
 
@@ -14,59 +13,15 @@ class  MatplotlibWidget ( QMainWindow ):
 
         loadUi ( "v4.ui" , self )
 
-        self . setWindowTitle ( "Plot Graph" )
-
-        self . generate1 . clicked . connect ( self . update_graph1 )
-        self . generate2 . clicked . connect ( self . update_graph2 )
-        self . generate3 . clicked . connect ( self . update_graph3 )
-        self . generate4 . clicked . connect ( self . update_graph4 )
+        self . GraphButton1 . clicked . connect ( self . MplWidget1 . update_graph )
+        self . GraphButton2 . clicked . connect ( self . MplWidget2 . update_graph )
+        self . GraphButton3 . clicked . connect ( self . MplWidget3 . update_graph )
+        self . GraphButton4 . clicked . connect ( self . MplWidget4 . update_graph )
 
         self . addToolBar ( NavigationToolbar ( self . MplWidget1 . canvas ,  self ))
         self . addToolBar ( NavigationToolbar ( self . MplWidget2 . canvas ,  self ))
         self . addToolBar ( NavigationToolbar ( self . MplWidget3 . canvas ,  self ))
         self . addToolBar ( NavigationToolbar ( self . MplWidget4 . canvas ,  self ))
-
-
-    def  update_graph1 ( self ):
-
-        n_data = 50
-        xdata = list(range(n_data))
-        ydata = [random.randint(0, 10) for i in range(n_data)]
-
-        self . MplWidget1 . canvas . axes . clear ()
-        self . MplWidget1 . canvas . axes . plot (xdata, ydata, 'r' )
-        self . MplWidget1 . canvas . draw ()
-
-    def  update_graph2 ( self ):
-
-        n_data = 50
-        xdata = list(range(n_data))
-        ydata = [random.randint(0, 10) for i in range(n_data)]
-
-        self . MplWidget2 . canvas . axes . clear ()
-        self . MplWidget2 . canvas . axes . plot (xdata, ydata, 'r' )
-        self . MplWidget2 . canvas . draw ()
-
-    def  update_graph3 ( self ):
-
-        n_data = 50
-        xdata = list(range(n_data))
-        ydata = [random.randint(0, 10) for i in range(n_data)]
-
-        self . MplWidget3 . canvas . axes . clear ()
-        self . MplWidget3 . canvas . axes . plot (xdata, ydata, 'r' )
-        self . MplWidget3 . canvas . draw ()
-
-    def  update_graph4 ( self ):
-
-        n_data = 50
-        xdata = list(range(n_data))
-        ydata = [random.randint(0, 10) for i in range(n_data)]
-
-        self . MplWidget4 . canvas . axes . clear ()
-        self . MplWidget4 . canvas . axes . plot (xdata, ydata, 'r' )
-        self . MplWidget4 . canvas . draw ()
-
 
 app  =  QApplication ([])
 window  =  MatplotlibWidget ()
--- Emulator/xian/mplwidget.py
+++ Emulator/xian/mplwidget.py
@@ -4,6 +4,8 @@ from  matplotlib.backends.backend_qt5agg  import  FigureCanvas
 
 from  matplotlib.figure  import  Figure
 
+import  numpy  as  np
+import  random
 
 class  MplWidget ( QWidget ):
 
@@ -18,3 +20,13 @@ class  MplWidget ( QWidget ):
 
         self . canvas . axes  =  self . canvas . figure . add_subplot ( 111 )
         self . setLayout ( vertical_layout )
+
+    def  update_graph ( self ):
+
+        n_data = 50
+        xdata = list(range(n_data))
+        ydata = [random.randint(0, 10) for i in range(n_data)]
+
+        self . canvas . axes . clear ()
+        self . canvas . axes . plot (xdata, ydata, 'r' )
+        self . canvas . draw ()
